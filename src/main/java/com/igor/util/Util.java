5bd79e9461f0119eb23a2e77e3a05fd0f61652f5
Igor Holyak <sportov.net@gmail.com>
2015-10-06 20:13:44 +0300

    Add util

---
+++ src/main/java/com/igor/util/Util.java
@@ -0,0 +1,11 @@
+package com.igor.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: igor
+ * Date: 06.10.15
+ * Time: 20:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class Util {
+}
